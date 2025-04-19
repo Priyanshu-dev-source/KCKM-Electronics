94f305d04edebfba92618a7b4f2c5441e4781e76
Priyanshu-dev-source <priyanshu2004ss@gmail.com>
2025-04-20 05:14:48 +0530

    accomplished

--- app/components/mapContainer.jsx
+++ app/components/mapContainer.jsx
@@ -1,5 +1,48 @@
-// components/MapComponent.jsx
+
+// "use client";
+
+// import React from "react";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import L from "leaflet";
+// import "leaflet/dist/leaflet.css";
+// import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+// import markerIcon from "leaflet/dist/images/marker-icon.png";
+// import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// delete L.Icon.Default.prototype._getIconUrl;
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: markerIcon2x,
+//   iconUrl: markerIcon,
+//   shadowUrl: markerShadow,
+// });
+
+// const MapComponent = ({ latLng }) => {
+//   return (
+//     <MapContainer
+//       className="rounded-[20px]"
+//       center={latLng}
+//       zoom={16}
+//       scrollWheelZoom={true}
+//       style={{ height: "100%", width: "100%" }}
+//     >
+//       <TileLayer
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         attribution="&copy; OpenStreetMap contributors"
+//       />
+//       <Marker position={latLng}>
+//         <Popup>
+//           Selected Location <br /> ({latLng.lat}, {latLng.lng})
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
+//   );
+// };
+
+// export default MapComponent;
+
+
 "use client";
+
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
@@ -8,11 +51,15 @@ import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
+// Custom icon instance
+const customIcon = new L.Icon({
+  iconUrl: markerIcon.src || markerIcon,
+  iconRetinaUrl: markerIcon2x.src || markerIcon2x,
+  shadowUrl: markerShadow.src || markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
 });
 
 const MapComponent = ({ latLng }) => {
@@ -28,7 +75,7 @@ const MapComponent = ({ latLng }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
-      <Marker position={latLng}>
+      <Marker position={latLng} icon={customIcon}>
         <Popup>
           Selected Location <br /> ({latLng.lat}, {latLng.lng})
         </Popup>
